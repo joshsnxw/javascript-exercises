71445f41e2550983bf6e19fbbc31c4f9ad6d914e
joshsnxw <napkins.effects-0k@icloud.com>
2024-11-05 00:57:05 +0100

    Add exercises 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    if (cleanedStr === cleanedStr.split('').reverse().join('')) return true;
+    return false;
 };
 
 // Do not edit below this line
